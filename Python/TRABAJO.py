4d7daa262c3718711b2ad4804409acc2a81e3230
Vicente Enrique Aquino Ticse <vicente.eat@gmail.com>
2024-08-24 18:04:48 -0500

    scripts iniciales para el proyecto parcial

---
+++ Python/TRABAJO.py
@@ -0,0 +1,30 @@
+import os
+
+#### Carpeta dataset ####
+location = "C:\Documentos_Certus\Proyecto_Parcial\Python\dataset"
+
+### Validar si existe la carpeta ####
+if not os.path.exists(location): ## si no existe carpeta
+    ## creo carpeta
+    os.mkdir(location)
+else: ## si existe carpeta
+    ## borramos contenido
+    for root, dirs, files in os.walk (location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name)) ## eliminar archivos
+        for name in dirs:
+            os.rmdir(os.path.join(root,name)) ## eliminar carpeta
+
+
+#### Descargar dataset ####
+from kaggle.api.kaggle_api_extended import KaggleApi # type: ignore
+
+### Autenticarnos ###
+api = KaggleApi()
+api.authenticate()
+
+##  print(api.dataset_list(search=''))  
+
+api.dataset_download_files('youssefismail20/olympic-games-1994-2024', path=location, force=True, quiet=False, unzip=True)
+
+
